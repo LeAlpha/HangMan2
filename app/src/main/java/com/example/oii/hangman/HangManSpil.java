73d341531a814d5cb50e7a5196392dc159aa8f60
Stephan <Stephankhader@gmail.com>
2018-11-05 15:26:31 +0100

    Implementeret Async, med nye ord fra DR

--- app/src/main/java/com/example/oii/hangman/HangManSpil.java
+++ app/src/main/java/com/example/oii/hangman/HangManSpil.java
@@ -1,6 +1,7 @@
 package com.example.oii.hangman;
 
 import android.content.DialogInterface;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
@@ -9,11 +10,12 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class HangManSpil extends AppCompatActivity implements View.OnClickListener {
     Logic gameLogic = new Logic();
-    Button goKnap;
+    Button goKnap, nyeOrd;
     EditText inputFelt;
     TextView wordSoFar, usedLetters;
     ImageView picture;
@@ -27,6 +29,7 @@ public class HangManSpil extends AppCompatActivity implements View.OnClickListen
 
 
         goKnap = findViewById(R.id.GoButton);
+        nyeOrd = findViewById(R.id.newWords);
         inputFelt = findViewById(R.id.inputField);
         wordSoFar = findViewById(R.id.showWord);
         usedLetters = findViewById(R.id.usedLettersField);
@@ -35,11 +38,37 @@ public class HangManSpil extends AppCompatActivity implements View.OnClickListen
         onReload();
 
         goKnap.setOnClickListener(this);
+        nyeOrd.setOnClickListener(this);
 
     }
 
     @Override
     public void onClick(View v) {
+
+
+        class async extends AsyncTask{
+
+            @Override
+            protected Object doInBackground(Object[] objects) {
+                try {
+                    nyeOrd.setText("henter");
+                    gameLogic.hentOrdFraDr();
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                return null;
+            }
+        @Override
+        protected void onPostExecute(Object result) {
+                nyeOrd.setText("Nye ord");
+                gameLogic.nulstil();
+                onReload();
+            Toast.makeText(HangManSpil.this, "Nye ord er hentet :) ", Toast.LENGTH_SHORT).show();
+            }
+        }
+
+
         if (v == goKnap) {
 
             gameLogic.gætBogstav(inputFelt.getText().toString());
@@ -55,7 +84,7 @@ public class HangManSpil extends AppCompatActivity implements View.OnClickListen
                 AlertDialog.Builder builder = new AlertDialog.Builder(HangManSpil.this);
                 builder.setCancelable(false);
                 if(gameLogic.erSpilletTabt() == true){
-                    builder.setTitle("U DED Boii");
+                    builder.setTitle("U DED ");
                     builder.setMessage("Ordet var: " + gameLogic.getOrdet() + "\nTryk på knappen for at spille igen");
                     builder.setNegativeButton("Nyt Spil", new DialogInterface.OnClickListener() {
                         @Override
@@ -68,7 +97,7 @@ public class HangManSpil extends AppCompatActivity implements View.OnClickListen
                 }
                 else if(gameLogic.erSpilletVundet() == true){
 
-                    builder.setTitle("Yay! Du vandt! ");
+                    builder.setTitle("Yay! Du vandt! Ordet var:  " + gameLogic.getOrdet());
                     builder.setMessage("Tryk på knappen for at spille igen");
 
                     builder.setPositiveButton("Nyt Spil", new DialogInterface.OnClickListener() {
@@ -83,6 +112,12 @@ public class HangManSpil extends AppCompatActivity implements View.OnClickListen
                 builder.show();
             }
         }
+
+        else if(v == nyeOrd){
+            new async().execute();
+
+        }
+
     }
 
     public void onReload(){
@@ -107,6 +142,7 @@ public class HangManSpil extends AppCompatActivity implements View.OnClickListen
             picture.setImageResource(R.drawable.forkert6);
         }
     }
+
 
 
 }
