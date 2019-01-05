bb71906bb039a507ac2ab5c972028b9dd9d162b4
LeAlpha <Stephankhader@gmail.com>
2019-01-05 17:43:10 +0100

    Recyclerview implementeret i highscore

--- app/src/main/java/com/example/oii/hangman/Highscore.java
+++ app/src/main/java/com/example/oii/hangman/Highscore.java
@@ -4,62 +4,62 @@ import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Size;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+
 
 public class Highscore extends AppCompatActivity implements View.OnClickListener {
 
     SharedPreferences pm;
-    TextView tv;
     Button update;
-    String hs1_name, hs2_name, hs3_name, hs1_word, hs2_word, hs3_word;
-    int hs1_value, hs2_value, hs3_value;
 
+
+    private ArrayList<String> word = new ArrayList<>();
+    private ArrayList<Integer> score = new ArrayList<>();
+    private ArrayList<String> player = new ArrayList<>();
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_highscore);
-
-
-
-        tv = findViewById(R.id.textfield_HighScore);
-        tv.setText(highscoreText());
-
+        loadHighScore();
+        RecyclerView recyclerView = findViewById(R.id.recyclerView);
+        RecyclerviewAdapter adapter = new RecyclerviewAdapter(word, score, player, this);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
         update = (Button) findViewById(R.id.updateButton);
         update.setOnClickListener(this);
     }
 
-    public String highscoreText(){
-        loadHighScore();
-        return "1. \t"+ hs1_name + "\t " + hs1_value + "\t" + hs1_word +"\n"
-                +"2. \t"+ hs2_name +"\t " + hs2_value + "\t" + hs2_word + "\n" +
-                "3. \t"+ hs3_name + "\t " + hs3_value + "\t" + hs3_word;
 
-
-    }
 
 
 
     public void loadHighScore() {
         pm = PreferenceManager.getDefaultSharedPreferences(this);
         SharedPreferences.Editor myEditor = pm.edit();
-        hs1_name = pm.getString("H1Name", "blankName");
-        hs1_word = pm.getString("H1Word", "BlankWord");
-        hs1_value = pm.getInt("H1Score", 000);
 
-        hs2_name = pm.getString("H2Name", "blankName");
-        hs2_word = pm.getString("H2Word", "BlankWord");
-        hs2_value = pm.getInt("H2Score", 000);
+        word.add(pm.getString("H1Word", "BlankWord"));
+        word.add(pm.getString("H2Word", "BlankWord"));
+        word.add(pm.getString("H3Word", "BlankWord"));
 
-        hs3_name = pm.getString("H3Name", "blankName");
-        hs3_word = pm.getString("H3Word", "BlankWord");
-        hs3_value = pm.getInt("H3Score", 000);
+        score.add(pm.getInt("H1Score", 000));
+        score.add(pm.getInt("H2Score", 000));
+        score.add(pm.getInt("H3Score", 000));
+
+        player.add(pm.getString("H1Name", "blankName"));
+        player.add(pm.getString("H2Name", "blankName"));
+        player.add(pm.getString("H3Name", "blankName"));
+
+
     }
 
 
---
+++ app/src/main/java/com/example/oii/hangman/RecyclerviewAdapter.java
@@ -0,0 +1,64 @@
+package com.example.oii.hangman;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import java.util.ArrayList;
+
+public class RecyclerviewAdapter extends RecyclerView.Adapter<RecyclerviewAdapter.ViewHolder> {
+
+    private ArrayList<String> word = new ArrayList<>();
+    private ArrayList<Integer> score = new ArrayList<>();
+    private ArrayList<String> player = new ArrayList<>();
+    private Context context;
+
+    public RecyclerviewAdapter(ArrayList<String> words, ArrayList<Integer> score, ArrayList<String> player, Context context) {
+        this.word = words;
+        this.score = score;
+        this.player = player;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.layout_listitem, viewGroup, false);
+        ViewHolder holder = new ViewHolder(view);
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder viewHolder, int i) {
+        viewHolder.text1.setText(word.get(i));
+        viewHolder.text2.setText(score.get(i).toString());
+        viewHolder.text3.setText(player.get(i));
+
+        if(i == 1){viewHolder.image.setImageResource(R.drawable.gold);}
+        if(i == 2){viewHolder.image.setImageResource(R.drawable.silver);}
+        if(i == 3){viewHolder.image.setImageResource(R.drawable.bronze);}
+    }
+
+    @Override
+    public int getItemCount() {
+        return word.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+        TextView text1, text2, text3;
+        ImageView image;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            image = itemView.findViewById(R.id.listImage);
+            text1 = itemView.findViewById(R.id.listTextWord);
+            text2 = itemView.findViewById(R.id.listTextPlayer);
+            text3 = itemView.findViewById(R.id.listTextScore);
+        }
+    }
+}
